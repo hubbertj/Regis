6b715bf07e9bb0d37483c00c0961fd42fd2dbc90
Jerum Hubbert <hubbertj@gmail.com>
2017-03-21 00:37:12 -0700

    completed week 1 assiment

--- 310/Week1/src/CS310Hubbert/Main.java
+++ 310/Week1/src/CS310Hubbert/Main.java
@@ -1,8 +1,195 @@
 package CS310Hubbert;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Jerum Hubbert
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("We in this mother fucker");
+
+		final String INPUT_FILENAME = "input/assn1input.txt";
+
+		BufferedReader br = null;
+		String line = "";
+		int lineCount = 0;
+
+		Realtor myRealtor = null;
+		Realtor aRealtor = null;
+
+		Property myProperty = null;
+		Property aProperty = null;
+
+		// Week 2
+
+		System.out.println("Running Test Set 1a...\n");
+		Realtor real = new Realtor("AA0123456", "Jerum", "hubbert", "702-743-6322", 20.6);
+		System.out.println(real.toString() + "\n");
+		System.out.println("Running Test Set 1b...\n");
+		Property prop = new Property("1920394", "19029842", "1700 Toltec Cir", "somewhere", "co", 80152, 5, 3.5, false,
+				299562.0);
+		System.out.println(prop.toString() + "\n");
+
+		System.out.println("Running Test Set 2a...");
+
+		try {
+			// format
+			// REALTOR,ADD,licenseNum,firstName,lastName,phoneNumber,commissionPercentage
+			// PROPERTY,ADD,mlsNum,licenseNum,streetAddr,city,state,zipCode,numBedrooms,
+			// numBathRooms,sold(Y/N),askingPrice
+
+			br = new BufferedReader(new FileReader(INPUT_FILENAME));
+			while ((line = br.readLine()) != null) {
+				// use comma as separator
+				String[] inputRead = line.split(",");
+				
+				if (lineCount == 0 && inputRead[0].equals("REALTOR")) {
+					if (inputRead[1].equals("ADD")) {
+						System.out.println("\n");
+						myRealtor = setRealtorAttributes(new Realtor(),
+								new String[] { inputRead[2], inputRead[3], inputRead[4], inputRead[5], inputRead[6] });
+						if (!myRealtor.isLicenseNumValid(myRealtor.getLicenseNum())) {
+							System.err.println("License number is invalid!");
+						}
+						if (!myRealtor.isPhoneNumValid(myRealtor.getPhoneNum())) {
+							System.err.println("Phone number is invalid!");
+						}
+
+						displayRealtorAttributes(myRealtor);
+					}
+				} else if (lineCount == 1 && inputRead[0].equals("REALTOR")) {
+					System.out.println("Running Test Set 2a...");
+					if (inputRead[1].equals("ADD")) {
+						System.out.println("\n");
+						aRealtor = setRealtorAttributes(new Realtor(),
+								new String[] { inputRead[2], inputRead[3], inputRead[4], inputRead[5], inputRead[6] });
+						if (!aRealtor.isLicenseNumValid(aRealtor.getLicenseNum())) {
+							System.err.println("License number is invalid!");
+						}
+						if (!aRealtor.isPhoneNumValid(aRealtor.getPhoneNum())) {
+							System.err.println("Phone number is invalid!");
+						}
+
+						displayRealtorAttributes(aRealtor);
+					}
+				} else if (lineCount == 2 && inputRead[0].equals("PROPERTY")) {
+					System.out.println("Running Test Set 2c...");
+					if (inputRead[1].equals("ADD")) {
+						System.out.println("\n");
+						myProperty = setPropertyAttributes(new Property(),
+								new String[] { inputRead[2], inputRead[3], inputRead[4], inputRead[5], inputRead[6],
+										inputRead[7], inputRead[8], inputRead[9], inputRead[10], inputRead[11] });
+
+						if (!myProperty.isMlsValid(myProperty.getMlsNum())) {
+							System.err.println("MLS number is not valid!");
+						}
+						if (!myProperty.isStateValid(myProperty.getState())) {
+							System.err.println("State must be \"WY\" or \"CO\" ");
+						}
+						if (!myProperty.isZipcodeValid(myProperty.getZipCode())) {
+							System.err.println("Zip code must start with 80, 81, 82, 83");
+						}
+
+						displayPropertyAttributes(myProperty);
+					}
+
+				} else if (lineCount == 3 && inputRead[0].equals("PROPERTY")) {
+					System.out.println("Running Test Set 2d...");
+					if (inputRead[1].equals("ADD")) {
+						System.out.println("\n");
+						aProperty = setPropertyAttributes(new Property(),
+								new String[] { inputRead[2], inputRead[3], inputRead[4], inputRead[5], inputRead[6],
+										inputRead[7], inputRead[8], inputRead[9], inputRead[10], inputRead[11] });
+
+						if (!aProperty.isMlsValid(aProperty.getMlsNum())) {
+							System.err.println("MLS number is not valid!");
+						}
+						if (!aProperty.isStateValid(aProperty.getState())) {
+							System.err.println("State must be \"WY\" or \"CO\" ");
+						}
+						if (!aProperty.isZipcodeValid(aProperty.getZipCode())) {
+							System.err.println("Zip code must start with 80, 81, 82, 83");
+						}
+
+						displayPropertyAttributes(aProperty);
+					}
+				}else{
+					break;
+				}
+
+				lineCount += 1;
+			}
+
+		} catch (FileNotFoundException e) {
+			System.err.println("cannot find input/assn1input.txt");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		System.out.println("\n");
+		System.out.println("Running Test Set 3a...\n");
+
+		if (myRealtor.equals(aRealtor)) {
+			System.out.println("Realtor objects are equal.\n");
+		} else {
+			System.out.println("Realtor objects are not equal!\n");
+		}
+		System.out.println("Running Test Set 3b...\n");
+		if(myProperty.equals(aProperty)){
+			System.out.println("Property objects are equal.\n");
+		}else{
+			System.out.println("Property objects are not equal!\n");
+		}
+
 	}
+
+	public static Realtor setRealtorAttributes(Realtor realtor, String[] arr) {
+		realtor.setLicenseNum(arr[0]);
+		realtor.setFirstName(arr[1]);
+		realtor.setLastName(arr[2]);
+		realtor.setPhoneNum(arr[3]);
+		realtor.setCommission(Double.parseDouble(arr[4]));
+		return realtor;
+	}
+
+	public static void displayRealtorAttributes(Realtor realtor) {
+		System.out.println(realtor.getLicenseNum() + "\n" + realtor.getFirstName() + "\n" + realtor.getLastName() + "\n"
+				+ realtor.getPhoneNum() + "\n" + realtor.getCommission());
+	}
+
+	public static Property setPropertyAttributes(Property property, String[] arr) {
+		property.setMlsNum(arr[0]);
+		property.setLicenseNum(arr[1]);
+		property.setStreetAddr(arr[2]);
+		property.setCity(arr[3]);
+		property.setState(arr[4]);
+		property.setZipCode(Integer.parseInt(arr[5]));
+		property.setBedrooms(Integer.parseInt(arr[6]));
+		property.setBathrooms(Double.parseDouble(arr[7]));
+		property.setSold(arr[8].equalsIgnoreCase("y"));
+		property.setAskingPrice(Double.parseDouble(arr[9]));
+
+		return property;
+	}
+
+	public static void displayPropertyAttributes(Property property) {
+		System.out.println(property.getMlsNum() + "\n" + property.getLicenseNum() + "\n" + property.getStreetAddr()
+				+ "\n" + property.getCity() + "\n" + property.getState() + "\n" + property.getZipCode() + "\n"
+				+ property.getSold() + "\n" + property.getAskingPrice());
+	}
+
 }
--- 310/Week1/src/CS310Hubbert/Realtor.java
+++ 310/Week1/src/CS310Hubbert/Realtor.java
@@ -1,5 +1,9 @@
 package CS310Hubbert;
 
+/**
+ *
+ * @author Jerum Hubbert
+ */
 public class Realtor {
 	private String licenseNum;
 	private String firstName;
@@ -10,6 +14,25 @@ public class Realtor {
 	public Realtor() {
 		this.init();
 	}
+
+	/**
+	 * This constructor requires all fields to be passed as parameters.
+	 * 
+	 * @param licenseNum
+	 *            - this is an String attribute.
+	 * 
+	 * @param firstName
+	 *            - this is a String attribute.
+	 * 
+	 * @param lastName
+	 *            - this is a String attribute.
+	 * 
+	 * @param phoneNum
+	 *            - this is an String attribute.
+	 * 
+	 * @param commission
+	 *            - this is a Double attribute.
+	 */
 
 	public Realtor(String licenseNum, String firstName, String lastName, String phoneNum, Double commission) {
 		super();
@@ -25,44 +48,42 @@ public class Realtor {
 
 	}
 
+	public Boolean isLicenseNumValid(String licenseNum) {
+		int licenseLength = licenseNum.length();
+
+		// Check if we format llnnnnnnn l=letter n=number
+		if (licenseLength != 9) {
+			return false;
+		} else if (!(Character.isLetter(licenseNum.charAt(0))) && !(Character.isLetter(licenseNum.charAt(1)))) {
+			return false;
+		} else if (!(Character.isDigit(licenseNum.charAt(2))) && !(Character.isDigit(licenseNum.charAt(4)))
+				&& !(Character.isDigit(licenseNum.charAt(5))) && !(Character.isDigit(licenseNum.charAt(6)))
+				&& !(Character.isDigit(licenseNum.charAt(7))) && !(Character.isDigit(licenseNum.charAt(8)))) {
+			return false;
+		}
+		return true;
+
+	}
+
+	public Boolean isPhoneNumValid(String phoneNum) {
+		// Check the phone number is 12 chars long and contains dashes and
+		String regexStr = "^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$";
+		// regex check for phone number
+		if (!phoneNum.matches(regexStr)) {
+			return false;
+		}
+		return true;
+	}
+
 	public String getLicenseNum() {
 		return licenseNum;
 	}
 
 	public void setLicenseNum(String licenseNum) {
-
-		int licenseLength = licenseNum.length();
-
-		// Check if we format llnnnnnnn l=letter n=number
-		if (licenseLength != 9) {
-			throw new ExceptionInInitializerError("License Number is incorrect!");
-		} else if (!(Character.isLetter(licenseNum.charAt(0))) && !(Character.isLetter(licenseNum.charAt(1)))) {
-			throw new ExceptionInInitializerError("License Number must start with two letters!");
-		} else if (!(Character.isDigit(licenseNum.charAt(2))) && !(Character.isDigit(licenseNum.charAt(4)))
-				&& !(Character.isDigit(licenseNum.charAt(5))) && !(Character.isDigit(licenseNum.charAt(6)))
-				&& !(Character.isDigit(licenseNum.charAt(7))) && !(Character.isDigit(licenseNum.charAt(8)))) {
-			throw new ExceptionInInitializerError("License Number must start with two letters!");
-		}
-
 		this.licenseNum = licenseNum;
 	}
 
 	public void setPhoneNum(String phoneNum) {
-		// Check the phone number is 12 chars long and contains dashes and
-		// digits in the correct places
-		
-		int licenseLength = phoneNum.length();
-
-		// Check if we format llnnnnnnn l=letter n=number
-		if (licenseLength != 9) {
-			throw new ExceptionInInitializerError("License Number is incorrect!");
-		} else if (!(Character.isLetter(licenseNum.charAt(0))) && !(Character.isLetter(licenseNum.charAt(1)))) {
-			throw new ExceptionInInitializerError("License Number must start with two letters!");
-		} else if (!(Character.isDigit(licenseNum.charAt(2))) && !(Character.isDigit(licenseNum.charAt(4)))
-				&& !(Character.isDigit(licenseNum.charAt(5))) && !(Character.isDigit(licenseNum.charAt(6)))
-				&& !(Character.isDigit(licenseNum.charAt(7))) && !(Character.isDigit(licenseNum.charAt(8)))) {
-			throw new ExceptionInInitializerError("License Number must start with two letters!");
-		}
 		this.phoneNum = phoneNum;
 	}
 
@@ -96,11 +117,23 @@ public class Realtor {
 
 	@Override
 	public boolean equals(Object obj) {
-		return super.equals(obj);
+		Realtor real = (Realtor) obj;
+		if (!real.getLicenseNum().equals(this.licenseNum)) {
+			return false;
+		} else if (!real.getFirstName().equals(this.firstName)) {
+			return false;
+		} else if (!real.getLastName().equals(this.lastName)) {
+			return false;
+		} else if (!real.getPhoneNum().equals(this.phoneNum)) {
+			return false;
+		} else if (!real.getCommission().equals(this.commission)) {
+			return false;
+		}
+		return false;
 	}
 
 	@Override
 	public String toString() {
-		return super.toString();
+		return licenseNum + ", " + firstName + ", " + lastName + ", " + phoneNum + ", " + commission.toString();
 	}
 }
