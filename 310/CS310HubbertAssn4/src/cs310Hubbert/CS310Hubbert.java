c8fa015c9d55af0e3f3e872ce2114f60a080ce3c
Jerum Hubbert <hubbertj@gmail.com>
2017-04-09 23:17:29 -0700

    adding more to week 4

--- 310/CS310HubbertAssn4/src/cs310Hubbert/CS310Hubbert.java
+++ 310/CS310HubbertAssn4/src/cs310Hubbert/CS310Hubbert.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 import cs310Hubbert.Car.CAR_TYPES;
 
