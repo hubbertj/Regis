1558504469db6a17532f8bdc9ff05e7bff3c83e1
Jerum Hubbert <hubbertj@gmail.com>
2017-05-01 22:04:57 -0700

    all done

--- 310/CS310HubbertSort/src/cs310Hubbert/CS310Hubbert.java
+++ 310/CS310HubbertSort/src/cs310Hubbert/CS310Hubbert.java
@@ -13,8 +13,9 @@ public class CS310Hubbert {
 	private static final String REPORT_OUTPUT = "output/sortResults.txt";
 
 	/**
+	 * Sorts a list of Integers using bubble sort
 	 * 
-	 * @param table
+	 * @param myList The list of Integer which are unordered.
 	 */
 	public static void bubbleSort(Integer[] myList) {
 		int pass = 1;
@@ -30,12 +31,11 @@ public class CS310Hubbert {
 				}
 			}
 		} while (exchange);
-//		System.out.println(Arrays.toString(myList));
 	}
 
-	/**
+	/**Sorts a list of Integers using insertion sort
 	 * 
-	 * @param myList
+	 * @param myList The list of Integer which are unordered.
 	 */
 	public static void insertionSort(Integer[] myList) {
 		for (int next = 1; next < myList.length; next++) {
@@ -46,14 +46,14 @@ public class CS310Hubbert {
 			}
 			myList[next] = nextval;
 		}
-//		System.out.println(Arrays.toString(myList));
 	}
 
-	/**
+	/**Sorts a list of Integers using merge sort
 	 * 
-	 * @param myList
+	 * @param myList The list of Integer which are unordered.
+	 * @param low The low number from the sub list.
+	 * @param high The high number from the sub list.
 	 */
-	/* Merge Sort function */
 	public static void mergeSort(Integer[] myList, int low, int high) {
 		int N = high - low;
 		if (N <= 1) {
@@ -83,14 +83,35 @@ public class CS310Hubbert {
 
 		for (int k = 0; k < N; k++) {
 			myList[low + k] = temp[k];
-//			if (k == myList.length - 1) {
-//				System.out.println(Arrays.toString(myList));
-//			}
+		}
+	}
+	
+	/**
+	 *  Prints out the body of the report
+	 * @param results A 3d array holding all the report data.
+	 */
+	public static void printReport(Long[][] results){
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(REPORT_OUTPUT, "UTF-8");
+			printReportHeader(writer);
+			printReportLine(writer, "Bubble Sort", results[0][0], results[1][0], results[2][0],
+					getAverage(results[0][0], results[1][0], results[2][0]));
+			printReportLine(writer, "Insertion Sort", results[0][1], results[1][1], results[2][1],
+					getAverage(results[0][1], results[1][1], results[2][1]));
+			printReportLine(writer, "Merge Sort", results[0][2], results[1][2], results[2][2],
+					getAverage(results[0][2], results[1][2], results[2][2]));
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("failed to read to file " + REPORT_OUTPUT);
+			e.printStackTrace();
 		}
 	}
 
 	/**
+	 * Prints out the header of the report
 	 * 
+	 * @param writer The Writer object used for writing to a file.
 	 */
 	public static void printReportHeader(PrintWriter writer) {
 		System.out.println("SORTING RESULTS");
@@ -98,34 +119,42 @@ public class CS310Hubbert {
 
 		System.out.println("---------------");
 		writer.println("---------------");
-
-		System.out.println("\t\tRun 1\tRun 2\tRun 3\tAverage");
-		writer.println("\t\t\t\tRun 1\tRun 2\tRun 3\tAverage");
+		
+		String format = "%-20s%-20s%-20s%-20s%-20s\n";
+		System.out.format(format,"", "Run 1", "Run 2", "Run 3", "Average");
+		writer.format(format,"", "Run 1", "Run 2", "Run 3", "Average");
 	}
 
-	/**
+	/** Prints out a report
 	 * 
+	 * @param writer The Writer object used for writing to a file.
+	 * @param sortName Name of the line of the report
+	 * @param runOneTimems First result run time.
+	 * @param runTwoTimems Second result run time.
+	 * @param runThreeTimems Third result run time.
+	 * @param avg The average of runtime for all results.
 	 */
 	public static void printReportLine(PrintWriter writer, String sortName, long runOneTimems, long runTwoTimems,
 			long runThreeTimems, long avg) {
-		System.out.print(sortName + "\t");
-		System.out.print(runOneTimems + "ns\t");
-		System.out.print(runTwoTimems + "ns\t");
-		System.out.print(runThreeTimems + "ns\t");
-		System.out.print(avg + "ns\t\n");
-
-		writer.print(sortName + "\t\t");
-		writer.print(runOneTimems + "ns\t");
-		writer.print(runTwoTimems + "ns\t");
-		writer.print(runThreeTimems + "ns\t");
-		writer.print(avg + "ns\t\n");
-
+		String format = "%-20s";
+		String format2 = "%-20s\n";
+		System.out.format(format, sortName);
+		System.out.format(format, runOneTimems + "ns");
+		System.out.format(format, runTwoTimems + "ns");
+		System.out.format(format, runThreeTimems + "ns");
+		System.out.format(format2, avg + "ns");
+		
+		writer.format(format, sortName);
+		writer.format(format, runOneTimems + "ns");
+		writer.format(format, runTwoTimems + "ns");
+		writer.format(format, runThreeTimems + "ns");
+		writer.format(format2, avg + "ns");
 	}
 
-	/**
+	/**Checks if a list if ordered.
 	 * 
-	 * @param myList
-	 * @return
+	 * @param myList The list of Integer which are unordered.
+	 * @return Boolean True if everything if the list passed is sorted.
 	 */
 	public static Boolean validatedSort(Integer[] myList) {
 		if (myList == null) {
@@ -142,61 +171,56 @@ public class CS310Hubbert {
 		}
 		return true;
 	}
-
-	public static long getAverage(Long[] times) {
-		long sum = 0;
-		for (long time : times) {
-			sum += time;
-		}
-		return (sum / (times.length + 1));
+	
+	/**
+	 * Takes in three time results and returns the average time.
+	 * 
+	 * @param result1 Time result 1
+	 * @param result2 Time result 2
+	 * @param result3 Time result 3
+	 * @return Long The average amount of time.
+	 */
+	public static long getAverage(Long result1, Long result2, Long result3) {
+		return ((result1 + result2 + result3) / 3);
 	}
 
 	/**
+	 * Main method runs three tests on different sort algorithms.
 	 * 
-	 * @param args
+	 * @param args arguments which can be passed in.
 	 */
 	public static void main(String[] args) {
 		final int LIST_RANGE = 100000;
 		int numElements = 50000;
+		Random random = new Random();
+		int sortCount = 1;
+		
+		Integer[] list1 = new Integer[numElements];
+		Integer[] list2 = new Integer[numElements];
+		Integer[] list3 = new Integer[numElements];
 
 		long bubbleSortNanoSeconds = System.nanoTime();
 		long insertionSortNanoSeconds = System.nanoTime();
 		long mergeSortNanoSeconds = System.nanoTime();
 
-		Long[] bubbleTimes, insertionTimes, mergeTimes;
-		//TODO: Create a 3x3 element 2D results arrays that will hold the time it takes to perform each sort 
-		bubbleTimes = new Long[3];
-		insertionTimes = new Long[3];
-		mergeTimes = new Long[3];
-
-		Integer[] list1;
-		Integer[] list2;
-		Integer[] list3;
-		Random random = new Random();
-
-		// Create the lists:
-		list1 = new Integer[numElements];
-		list2 = new Integer[numElements];
-		list3 = new Integer[numElements];
-
-		// Integer[] tempList = { 10, 20, 100, 1, 99, 76, 89 };
-		// Integer[] tempList1 = { 10, 20, 100, 50, 40, 11, 1, 99, 76, 89 };
-		// Integer[] tempList2 = { 10, 20, 100, 50, 40, 11, 1, 99, 76, 89, 99,
-		// 75, 67 };
-
-		for (int i = 0; i < bubbleTimes.length; i++) {
+		Long[][] results = new Long[3][];
+		results[0] = new Long[3];
+		results[1] = new Long[3];
+		results[2] = new Long[3];
+		
+		for (int i = 0; i < results.length; i++) {
 
 			// Initialize the values in each list:
 			for (int j = 0; j < numElements; j++) {
 				list1[j] = list2[j] = list3[j] = random.nextInt(LIST_RANGE);
 			}
 
-			System.out.println("Starting sort #1...");
+			System.out.println("Starting sort #"+ (sortCount++) +"...");
 			bubbleSortNanoSeconds = System.nanoTime();
 			bubbleSort(list1);
 			bubbleSortNanoSeconds = System.nanoTime() - bubbleSortNanoSeconds;
 			System.out.println("Bubble Sort time " + (bubbleSortNanoSeconds) + "ns");
-			bubbleTimes[i] = bubbleSortNanoSeconds;
+			results[i][0] = bubbleSortNanoSeconds;
 			if (!validatedSort(list1)) {
 				System.err.println(" Sort error in tempList");
 			} else {
@@ -205,12 +229,12 @@ public class CS310Hubbert {
 
 			System.out.println();
 
-			System.out.println("Starting sort #2...");
+			System.out.println("Starting sort #"+ (sortCount++) +"...");
 			insertionSortNanoSeconds = System.nanoTime();
 			insertionSort(list2);
 			insertionSortNanoSeconds = System.nanoTime() - insertionSortNanoSeconds;
 			System.out.println("Insertion Sort time " + (insertionSortNanoSeconds) + "ns");
-			insertionTimes[i] = insertionSortNanoSeconds;
+			results[i][1] = insertionSortNanoSeconds;
 			if (!validatedSort(list2)) {
 				System.err.println(" Sort error in tempList2");
 			} else {
@@ -219,12 +243,12 @@ public class CS310Hubbert {
 
 			System.out.println();
 
-			System.out.println("Starting sort #3...");
+			System.out.println("Starting sort #"+ (sortCount++) +"...");
 			mergeSortNanoSeconds = System.nanoTime();
 			mergeSort(list3, 0, list3.length);
 			mergeSortNanoSeconds = System.nanoTime() - mergeSortNanoSeconds;
 			System.out.println("Merge Sort time " + (mergeSortNanoSeconds) + "ns");
-			mergeTimes[i] = mergeSortNanoSeconds;
+			results[i][2] = mergeSortNanoSeconds;
 			if (!validatedSort(list3)) {
 				System.err.println(" Sort error in tempList1");
 			} else {
@@ -233,20 +257,8 @@ public class CS310Hubbert {
 
 			System.out.println();
 		}
-
-		try {
-			PrintWriter writer = new PrintWriter(REPORT_OUTPUT, "UTF-8");
-			printReportHeader(writer);
-			printReportLine(writer, "Bubble Sort", bubbleTimes[0], bubbleTimes[1], bubbleTimes[2],
-					getAverage(bubbleTimes));
-			printReportLine(writer, "Insertion Sort", insertionTimes[0], insertionTimes[1], insertionTimes[2],
-					getAverage(insertionTimes));
-			printReportLine(writer, "Merge Sort", mergeTimes[0], mergeTimes[1], mergeTimes[2], getAverage(mergeTimes));
-			writer.close();
-		} catch (IOException e) {
-			System.err.println("failed to read to file " + REPORT_OUTPUT);
-			e.printStackTrace();
-		}
+		
+		printReport(results);
 	}
 
 }
