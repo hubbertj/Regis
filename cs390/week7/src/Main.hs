29f0f7984fdb08a53944f6635b39b0c342b57f31
Jerum Hubbert <hubbertj@gmail.com>
2017-10-18 00:50:04 -0700

    completed week 7 assignment

--- cs390/week7/src/Main.hs
+++ cs390/week7/src/Main.hs
@@ -9,34 +9,6 @@ import System.Random
 -- 3. Write a function that takes an argument x and returns a lazy sequence that has every third number, starting with x.  Then, write a function that includes every fifth number, beginning with y.  Combine these functions through composition to return every eighth number, beginning with x+y.
 -- 4. Test this function using a variety of x and y values.
 
-
--- Write a sort that takes a list
-doMergeSort :: Ord a => [a] -> [a]
-doMergeSort [] = []
-doMergeSort [x] = [x]
-doMergeSort xs = mergeSort (doMergeSort (appendList xs)) (doMergeSort (prependList xs))
-
--- a function that compares its two arguments and then returns a sorted list
-mergeSort :: Ord a => [a] -> [a] -> [a]
-mergeSort xs [] = xs
-mergeSort [] ys = ys
-mergeSort (x:xs) (y:ys) 
-    | (x <= y)  = x:(mergeSort xs (y:ys)) 
-    | otherwise = y:(mergeSort (x:xs) ys)
-
-
--- Slips a list by x and returns the first part.
-cutList :: Int -> [a] -> [a]
-cutList z xs = take (length xs `div` z) xs
-
--- Slips a list by 2 and returns the first part.
-appendList :: [a] -> [a]
-appendList xs = take (length xs `div` 2) xs
-
--- Slips a list by 2 and returns the last part.
-prependList :: [a] -> [a]
-prependList xs = drop (length xs `div` 2) xs
-
 -- generates a IO Int 1 - 100
 randomInt :: IO Int
 randomInt = randomRIO (1, 100) :: IO Int
@@ -55,15 +27,27 @@ everyEightNum x y = zipWith (+) (thirdLazySequence x) $ fifthLazySequence y
 
 
 -- Write a sort that takes a list and a function that compares its two arguments and then returns a sorted list.
-sortListBy fun [] = []
-sortListBy fun (h:t) = insert fun h (sortListBy fun t)
+optionalSort :: (a -> a -> Bool) -> [a] -> [a]
+optionalSort fun [] = []
+optionalSort fun (x:xs) = insert fun x (optionalSort fun xs)
   where
   insert fun i [] = [i]
-  insert fun i (sh:st) = if (fun i sh) then i:sh:st else sh:(insert fun i st)
+  insert fun i (ix:ixs) = 
+    if (fun i ix) 
+        then i:ix:ixs 
+        else ix:(insert fun i ixs)
+
+aCompare :: Int -> Int -> Bool
+aCompare x y = 
+    if x > y
+        then False
+        else True
+
 
 
 main :: IO ()
 main = do
+    putStrLn(" ")
     putStrLn "Creating a [Int]"
     -- not sure if there is a way to generated a random list in pure functional programming other then this
     var <- randomInt
@@ -94,8 +78,13 @@ main = do
     print masterList
     putStrLn(" ")
 
+    -- putStrLn "Write a sort that takes a list and a function that compares its two arguments and then returns a sorted list"
+    -- let sortList = doMergeSort masterList
+    -- print sortList
+    -- putStrLn(" ")
+
     putStrLn "Write a sort that takes a list and a function that compares its two arguments and then returns a sorted list"
-    let sortList = doMergeSort masterList
+    let sortList = optionalSort aCompare masterList
     print sortList
     putStrLn(" ")
 
@@ -122,5 +111,36 @@ main = do
     let everyEightList = take 10 everyEightSequenceList
     print everyEightList
     putStrLn(" ")
+
+
+    putStrLn "Test this function using a variety of x and y values"
+    putStrLn(" ")
+
+    putStrLn "Write a function that takes an argument x and returns a lazy sequence that has every third number, starting with x."
+    randomNumx <- randomInt
+    putStrLn("Starting with " ++(show randomNumx))
+    let threeListSequenceList = thirdLazySequence randomNumx
+    let threeList = take 10 threeListSequenceList
+    print threeList
+    putStrLn(" ")
+
+    putStrLn "Write a function that includes every fifth number, beginning with y."
+    randomNumy <- randomInt
+    putStrLn("Starting with " ++(show randomNumy))
+    let fiveListSequenceList = fifthLazySequence randomNumy
+    let fiveList = take 10 fiveListSequenceList
+    print fiveList
+    putStrLn(" ")
+
+    putStrLn "Combine these functions through composition to return every eighth number, beginning with x + y."
+
+    putStrLn("Using " ++ (show randomNumx) ++ " and " ++ (show randomNumy) ++ " starting with " ++ (show (randomNumx + randomNumy)))
+    let everyEightSequenceList = everyEightNum randomNumx randomNumy
+    let everyEightList = take 10 everyEightSequenceList
+    print everyEightList
+    putStrLn(" ")
+
+
+    putStrLn (show $ aCompare 10 20)
 
     putStrLn "End"
