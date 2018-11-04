288c162aa9e8e7e23072284414fe801bf6ec5fa4
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2018-11-04 00:37:19 -0700

    adding files

--- CS431/week1/mycopy.c
+++ CS431/week1/mycopy.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+#include <stdlib.h>
+#define STRMAX 100
+
+void copy(char _source[STRMAX], char _destination[STRMAX])
 {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    char ch;
+    FILE *source, *target;
+
+    source = fopen(_source, "r");
+    target = fopen(_destination, "w");
+
+    if (!source)
+    {
+        printf("Failed to open file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("copying %s to %s\n", _source, _destination);
+
+    while ((ch = fgetc(source)) != EOF)
+    {
+        fputc(ch, target);
+    }
+
+    printf("file copied.\n");
+
+    fclose(source);
+    fclose(target);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char source[STRMAX];
+    char destination[STRMAX];
+
+    for(i = 1; i < argc; i++)
+    {
+        if(i == 1)
+        {
+            strncpy(source, argv[i], STRMAX);
+        }
+        else if(i == 2)
+        {
+            strncpy(destination, argv[i], STRMAX);
+        }
+    }
+
+    if (strlen(source) == 0 || strlen(destination) == 0)
+    {
+        printf("Error: Missing argument\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        copy(source, destination);
+    }
+    exit(EXIT_FAILURE);
 }
