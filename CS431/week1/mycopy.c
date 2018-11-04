b29a4524f39028736ac3b0cf5a8e3ecdc120b952
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2018-11-04 12:03:32 -0800

    adding more to assignment

--- CS431/week1/mycopy.c
+++ CS431/week1/mycopy.c
@@ -1,7 +1,38 @@
+/*H**********************************************************************
+* FILENAME : mycopy.c
+* UNIVERSITY: REGIS
+* CLASS: CS431
+*
+* DESCRIPTION :
+*       This programming project serves as an introduction or refresher to
+*       C programming and the use of basic system calls in Unix that we
+*       begin to discuss in Chapter 2 and will continue to use for the
+*       duration of the semester. Conceptually, the requirements of this
+*       assignment are quite simple. All you are asked to do is write a C
+*       program that copies the contents of a file from a source location
+*       to a destination, both specified as command line arguments. That
+*       objective is straighforward enough, but a recurring theme in this
+*       course and in systems design in general is the handling of errors
+*       and unexpected conditions as they arise. Therefore, the intent is
+*       for you to write code that not only functions properly in the ideal
+*       case, but also can gracefully and correctly handle error conditions.
+*
+*
+* PUBLIC FUNCTIONS :
+*       void     file_copy( source, destination )
+*
+*
+* AUTHOR :    Jerum Lee Hubbert       START DATE :    04 Nov 2018
+*
+*
+*H*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
 #define STRMAX 100
+
 typedef enum { false, true } bool;
 
 void file_copy(char _source[STRMAX], char _destination[STRMAX])
@@ -15,15 +46,23 @@ void file_copy(char _source[STRMAX], char _destination[STRMAX])
 
     if (!source)
     {
-        printf("could not open file %s:  No such file or directory\n", _source);
+        if(errno == EPERM)
+        {
+            printf("Error: Permission Denied\n");
+        }
+        else if(errno == ENOENT)
+        {
+            printf("could not open file %s:  No such file or directory\n", _source);
+        }
+        else
+        {
+            printf("Error: Unknown Error\n");
+        }
         exit(EXIT_FAILURE);
     }
 
-    // gets file size
-    fseek(source, 0, SEEK_END);
-    size = ftell(source);
-    fseek(source, 0, SEEK_SET);
-  
+
+
     printf("copying %s to %s\n", _source, _destination);
 
     while ((ch = fgetc(source)) != EOF)
@@ -31,6 +70,10 @@ void file_copy(char _source[STRMAX], char _destination[STRMAX])
         fputc(ch, target);
     }
 
+    // gets file size of target
+    fseek(target, 0, SEEK_END);
+    size = ftell(target);
+    fseek(target, 0, SEEK_SET);
     printf("copied %d bytes from file %s to %s.\n", size, _source, _destination);
 
     fclose(source);
